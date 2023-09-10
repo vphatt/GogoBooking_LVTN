732f7b182142860b2bafe2019d7c901de8498f9e
phatB1910276 <phatB1910276@student.ctu.edu.vn>
2023-09-10 11:02:33 +0700

    Ket noi ios den firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
